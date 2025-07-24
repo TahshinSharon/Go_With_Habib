c7545d05f4c31770ce238746ad9f374cc99424b6
TahshinSharon <atsharon88@gmail.com>
2025-07-24 13:15:41 +0600

    Complete Lecture 40 Separate Stack For Separate Thread!

---
+++ lecture/Lec40/lec40.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 core have 2 virtual cpu/processor!")
+	fmt.Println("cpu executes os!")
+	fmt.Println("Os Karnel control Process and one process may have Multiple Threds!")
+	fmt.Println("Separate Stack For Separate Thread!")
+	fmt.Println("Thread created by programmer!")
+}
