09f2d83e6c9e17552af82a1cd03b3a643c4621e6
TahshinSharon <atsharon88@gmail.com>
2025-07-25 12:52:14 +0600

    Thread

---
+++ lecture/Lec38/lec38.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Threds")
+	fmt.Println("Cpu Threads are called the virtual processor one core have 2 virtual processor/threads")
+	fmt.Println("Process Threads are called virtual process one process may have multiple virtual process/threads")
+
+}
