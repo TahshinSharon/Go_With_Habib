d1f5a197c3877ba5ae50df0a238cfc098173261f
TahshinSharon <atsharon88@gmail.com>
2025-07-29 23:40:39 +0600

    Complete Lec42 Beautiful Go Routine

---
+++ lecture/Lec42/lec42.go
@@ -0,0 +1,20 @@
+package main
+//Complex And Beautiful Go Routine
+import(
+	"fmt"
+	"time"
+)
+var a=10
+const p =11
+func printHello(num int){
+	fmt.Println("Hello Habib",num)
+}
+func main(){
+	go printHello(1)
+	go printHello(2)
+	go printHello(3)
+	go printHello(4)
+	go printHello(5)
+	fmt.Println(a," ",p)
+	time.Sleep(2*time.Second)
+}
