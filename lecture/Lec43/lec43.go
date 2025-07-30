5ec4faaa4caed12ac10dba7bf801c3bcc2030977
TahshinSharon <atsharon88@gmail.com>
2025-07-31 00:07:02 +0600

    Lecture 43 Into The Backend Development

---
+++ lecture/Lec43/lec43.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Into The Backend Development
+func main() {
+	fmt.Println("Web 1.0/Static")
+	fmt.Println("Web 2.0/Dynamic")
+	fmt.Println("Web3.0")
+	fmt.Println("Api")
+	fmt.Println("Rest Api -> Represantional State Transfer-> State/ Resourse/Entity/Concept")
+}
