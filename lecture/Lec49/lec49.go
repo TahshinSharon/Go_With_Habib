66537a8259a0540597e2e35f3f48c21a13c250f4
TahshinSharon <atsharon88@gmail.com>
2025-09-12 12:35:08 +0600

    Lecture 49 Preflight Request

---
+++ lecture/Lec49/lec49.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("OPTIONS-Preflight-Request")
+	/*
+			Preflight Request
+			OPTIONS
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		    w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Tahshin")
+		    if r.Method == "OPTIONS" {
+			    w.WriteHeader(200)
+			    return
+		    }
+	*/
+}
